1974461aeb844a12037848fcb5e684d22b47a031
unknown <mouchine3@gmail.com>
2026-04-22 10:42:54 +0100

    first commit

---
+++ app/menu/pates/page.jsx
@@ -0,0 +1,14 @@
+import NavMenu from "../../components/NavMenu";
+import PatesHero from "./components/PatesHero";
+import PatesMenu from "./components/PatesMenu";
+
+
+export default function Home() {
+  return (
+    <main>
+      <NavMenu />
+      <PatesHero />
+      <PatesMenu />
+    </main>
+  );
+}
